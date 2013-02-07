48691018590d8e40c97552629d371fca3aef1d7d
kremenso <kremenso@o2.pl>
2013-02-07 19:33:35 +0100

    Changes

--- app/models/bigos_events_box/event_option.rb
+++ app/models/bigos_events_box/event_option.rb
@@ -1,5 +1,6 @@
 module BigosEventsBox
   class EventOption < ActiveRecord::Base
     attr_accessible :description, :event_id, :name, :price
+    belongs_to :event
   end
 end
--- app/uploaders/bigos_events_box/event_image_uploader.rb
+++ app/uploaders/bigos_events_box/event_image_uploader.rb
@@ -13,7 +13,6 @@ module BigosEventsBox
   end
 
   version :thumb do
-    #process :resize_to_fill => [BigosEventsBox::Setting["#{BigosEventsBox.name}.thumb_width"].to_i, BigosEventsBox::Setting["#{BigosEventsBox.name}.thumb_height"].to_i]
     process :resize_to_fill => [275, 45]
   end
 
